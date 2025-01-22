16cd01cea2645c7ed8004554b5ea1c975869674e
Nischal Saini <annunikkusaini123@gmail.com>
2025-01-23 01:09:41 +0530

    Fixed server.js file and updated project structure

--- server.js
+++ server.js
@@ -2,24 +2,26 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
-import { connect } from "mongoose";
 import connectDB from "./config/mongodb.js";
-import authRouter from './routes/authRoutes.js'
+import authRouter from './routes/authRoutes.js';
 import userRouter from "./routes/userRoutes.js";
 
 const app = express();
-const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173','http://localhost:5174','https://feauth-abbhf1vtk-nischal-sainis-projects.vercel.app']
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'https://feauth-abbhf1vtk-nischal-sainis-projects.vercel.app'
+];
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 
-// API EndPoints
-app.get('/', (req, res) => res.send("API Working fine")); // First API EndPoint
+// API Endpoints
+app.get('/', (req, res) => res.send("API Working fine"));
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 
-app.listen(port, () => console.log(`Server started on port: ${port}`));
+export default app; 
